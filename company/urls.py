2823d913346f905bfea70efa372de899bbf78778
unknown <Md Shipon Miah>
2025-02-24 22:23:25 +0600

    Added contact-form path to url patterns

--- company/urls.py
+++ company/urls.py
@@ -19,9 +19,11 @@ from django.contrib import admin
 from django.urls import path
 from app.views import (
     index,
+    contact_form,
 )
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', index, name="home"),
+    path('contact/', contact_form, name="contact_form"),
 ]
