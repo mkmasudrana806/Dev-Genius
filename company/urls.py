88c8f281098cf901abe82c5c317d098a217a8a1c
unknown <Md Shipon Miah>
2025-02-24 21:29:04 +0600

    Added home path to url patterns and registered new app in settings

--- company/urls.py
+++ company/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from app.views import (
+    index,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', index, name="home"),
 ]
