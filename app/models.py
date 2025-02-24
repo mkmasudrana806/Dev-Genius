cc86450458213f4fc97a9ae41ecb647e5634c44d
mkmasudrana806 <mkmasudrana806@gmail.com>
2025-02-24 22:26:23 +0600

    Added contact form

--- app/models.py
+++ app/models.py
@@ -17,3 +17,18 @@ class GeneralInfo(models.Model):
         return self.company_name
     # class Meta:
     #     db_table = "custom_table_name"
+
+
+
+class ContactFormLog(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+    action_time = models.DateTimeField(null=True, blank=True)
+    is_success = models.BooleanField(default=False)
+    is_error = models.BooleanField(default=False)
+    error_message = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.email
